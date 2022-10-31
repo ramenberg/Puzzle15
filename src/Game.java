d8b4aa7cfcec636b1209b123d3d0aeff6c846145
Emelie Ramenberg <ramenberg@gmail.com>
2022-10-31 11:03:42 +0100

    Game change

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+
 public class Game {
+
+    // TODO global variables
+    int[][] board = new int[4][4];
+    int tile = 0;
+
+    // TODO GUI at the top
+
+    public Game() {
+
+    // TODO board: fixa att inte nr 16 skrivs ut
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                    board[i][j] = tile;
+                    tile++;
+            }
+        }
+        System.out.println(Arrays.deepToString(board)); // test för att skriva ut 2d array
+    }
 }
