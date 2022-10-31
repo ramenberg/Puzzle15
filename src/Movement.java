2b51faf3220a7ed2382ec6132cfe8b6ead7c3c60
Emelie Ramenberg <ramenberg@gmail.com>
2022-10-31 11:17:52 +0100

    Movement init

--- src/Movement.java
+++ src/Movement.java
@@ -1,2 +1,23 @@
 public class Movement {
+    public Movement(int position) {
+        //Get the indexes where the block is, e.g. getPositions() and hold them on some vars, e.g. row, col
+        boolean moved = false;
+
+        if (row - 1 > 0 && board[row - 1][col] == 0) {  // Move block up
+            board[row - 1][col] = position;
+            moved = true;
+        } else if (row + 1 < gridLength - 1 && board[row + 1][col] == 0) { // Move block down
+            board[row + 1][col] = position;
+            moved = true;
+        } else if (col - 1 > 0 && board[row][col - 1] == 0) {  // Move block left
+            board[row][col - 1] = position;
+            moved = true;
+        } else if (col + 1 < gridLength - 1 && board[row][col + 1] == 0) { // Move block right
+            board[row][col + 1] = position;
+            moved = true;
+        }
+
+        if moved
+        board[row][col] = 0 // Delete the piece from old position
+    }
 }
