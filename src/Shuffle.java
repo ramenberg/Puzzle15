bf3b7b2be1c1c70bba3b0e758caf51c5e7583ea3
Ziggelito <Sixten.Lonnberg@yh.nackademin.se>
2022-10-31 15:08:37 +0100

    Shuffle

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -1,2 +1,10 @@
+import java.util.Collections;
+
 public class Shuffle {
+public void shuffleTiles() {
+    if(tiles != null){
+        Collections.shuffle(Arrays.asList(tiles));
+        layoutTiles();
+    }
+}
 }
