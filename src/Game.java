ff90a3a358877947bd81c13c4f844b2503ade2ba
Emelie Ramenberg <ramenberg@gmail.com>
2022-11-02 10:28:10 +0100

    Win feature

--- src/Game.java
+++ src/Game.java
@@ -162,32 +162,45 @@ public class Game extends JFrame implements GameInterface {
 //                }
 //            }
 
-        // Movement
-        public void Movement(int x,int y) {
+    // Movement
+    public void Movement(int x,int y) {
 
-            boolean moved = false;
-            int row = y/110;
-            int col = x/122;
-            int position = board[row][col];
-            System.out.println((row) + " " + (col)); // TODO ta bort innan final
+        boolean moved = false;
+        int row = y/110;
+        int col = x/122;
+        int position = board[row][col];
+        System.out.println((row) + " " + (col)); // TODO ta bort innan final
 
-            if (row > 0 && board[row - 1][col] == 0) {  // Move UP
-               board[row - 1][col] = position;
-               moved = true;
-           } else if (row < boardSize - 1 && board[row + 1][col] == 0) { // Move DOWN
-               board[row + 1][col] = position;
-                moved = true;
-           } else if (col > 0 && board[row][col - 1] == 0) {  // Move LEFT
-               board[row][col - 1] = position;
-               moved = true;
-            } else if (col < boardSize - 1 && board[row][col + 1] == 0) { // Move RIGHT
-                board[row][col + 1] = position;
-                moved = true;
-            }
-            if (moved)
-                board[row][col] = 0; // Delete from old position
+        if (row > 0 && board[row - 1][col] == 0) {  // Move UP
+           board[row - 1][col] = position;
+           moved = true;
+       } else if (row < boardSize - 1 && board[row + 1][col] == 0) { // Move DOWN
+           board[row + 1][col] = position;
+            moved = true;
+       } else if (col > 0 && board[row][col - 1] == 0) {  // Move LEFT
+           board[row][col - 1] = position;
+           moved = true;
+        } else if (col < boardSize - 1 && board[row][col + 1] == 0) { // Move RIGHT
+            board[row][col + 1] = position;
+            moved = true;
+        }
+        if (moved)
+            board[row][col] = 0; // Delete from old position
 
-            renderBoard();
+        renderBoard();
+        if (gameWon(board)) {
+            JOptionPane.showMessageDialog(null, "Du har vunnit!\nTryck Ok för att spela igen");
+            newBoard();
             }
         }
     }
+    public boolean gameWon(int[][] board) {
+        int[][] winArray = new int[][]{
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,0}
+        };
+        return Arrays.deepEquals(board, winArray);
+    }
+}
